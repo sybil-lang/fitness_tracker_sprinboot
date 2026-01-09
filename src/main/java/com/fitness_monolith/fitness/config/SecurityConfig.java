b0f4d45a3a35cd2d5653b439195f2d7a01820d6e
sybil-lang <78490279+sybil-lang@users.noreply.github.com>
2026-01-09 16:02:36 +0530

    Database Authentication: MySQL Integration with Spring Security

--- src/main/java/com/fitness_monolith/fitness/config/SecurityConfig.java
+++ src/main/java/com/fitness_monolith/fitness/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.fitness_monolith.fitness.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,12 +11,17 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
@@ -30,15 +36,35 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user1= User.withUsername("user1")
-                .password("{noop}user1")
-                .roles("USER")
-                .build();
-        UserDetails user2= User.withUsername("admin")
-                .password("{noop}admin")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user1,user2);
+//        UserDetails user1= User.withUsername("user1")
+//                .password("{noop}user1")
+//                .roles("USER")
+//                .build();
+//        UserDetails user2= User.withUsername("admin")
+//                .password("{noop}admin")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(user1,user2);
+
+        JdbcUserDetailsManager manager =
+                new JdbcUserDetailsManager(dataSource);
+
+        if (!manager.userExists("user1")) {
+            manager.createUser(User.withUsername("user1")
+                    .password("{noop}user1")
+                    .roles("USER")
+                    .build());
+        }
+
+        if (!manager.userExists("admin")) {
+            manager.createUser(User.withUsername("admin")
+                    .password("{noop}admin")
+                    .roles("ADMIN")
+                    .build());
+        }
+
+        return manager;
+
     }
 }
 
